aa826aafc89d5e5cc0f46c2a9f1a3b74bbe0bd56
xrosilloprofe <xv.rosillo@edu.gva.es>
2022-12-20 18:56:25 +0100

    0.2

--- src/main/java/es/ieslavereda/cartas/CartasTester.java
+++ src/main/java/es/ieslavereda/cartas/CartasTester.java
@@ -3,8 +3,11 @@ package es.ieslavereda.cartas;
 public class CartasTester {
     public static void main(String[] args) {
         Baraja baraja = new Baraja();
+        Jugador j1 = new Jugador("Pepa");
+
+        baraja.shuffle();
         System.out.println(baraja.toString());
     }
 }
 
-// https://github.com/ieslavereda/Cartas
+//
---
+++ src/main/java/es/ieslavereda/cartas/Jugador.java
@@ -0,0 +1,20 @@
+package es.ieslavereda.cartas;
+
+public class Jugador {
+
+
+    nombre del jugador
+    array de cartas llamado mano
+
+    constructor --> pasarle el nombre
+    y una mano con un array de 0 cartas
+
+    setters y getters? --> getNombre
+
+    toString
+
+            añadir carta
+
+                    puntuación
+
+}
