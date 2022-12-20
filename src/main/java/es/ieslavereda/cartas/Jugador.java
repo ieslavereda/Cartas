e20491405c07309c0e913967f98b3ba194460c10
xrosilloprofe <xv.rosillo@edu.gva.es>
2022-12-20 21:03:15 +0100

    0.3

--- src/main/java/es/ieslavereda/cartas/Jugador.java
+++ src/main/java/es/ieslavereda/cartas/Jugador.java
@@ -34,14 +34,30 @@ public class Jugador {
     }
 
     public int getPuntuacion(){
-        int points = 0;
-//        for (int i = 0; i < mano.length; i++) {
-//            points = points + mano[i].getNumero().getPuntuacion()[0];
-//        }
-        for (Carta carta: mano)
-            points += carta.getNumero().getPuntuacion()[0];
+        return getPuntuacionRecursivo(0,0);
+//        int points = 0;
+////        for (int i = 0; i < mano.length; i++) {
+////            points = points + mano[i].getNumero().getPuntuacion()[0];
+////        }
+//        for (Carta carta: mano)
+//            points += carta.getNumero().getPuntuacion()[0];
+//
+//        return (points>21) ? -1 : points;
 
-        return (points>21) ? -1 : points;
+    }
+
+    public int getPuntuacionRecursivo(int index, int sum) {
+        if(sum>21) return -1;
+        if(index>= mano.length)
+            return sum;
+        if (mano[index].getNumero().getPuntuacion().length == 1)
+            return getPuntuacionRecursivo(index+1,sum+mano[index].getNumero().getPuntuacion()[0]);
+
+        return Math.max(
+                getPuntuacionRecursivo(index+1,sum+mano[index].getNumero().getPuntuacion()[0]),
+                getPuntuacionRecursivo(index+1,sum+mano[index].getNumero().getPuntuacion()[1])
+        );
+
     }
 
 }
